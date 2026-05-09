5743651fa833e5ab10defc9680f8952fa3d08e38
Dominik <dominik.turczynski@gmail.com>
2026-05-09 13:07:29 +0200

    Begin work on payment list implementation

---
+++ src/employeeSystem/paymentList.cpp
@@ -0,0 +1,8 @@
+#include <wtts/paymentList.hpp>
+
+namespace es {
+void PaymentList::addPayment(Record) {}
+void PaymentList::printData() const {}
+
+void PaymentList::updatePayment(Employee const *const, double const newValue) {}
+} // namespace es
