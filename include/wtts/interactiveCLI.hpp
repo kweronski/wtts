5f6967f65fb7225da98d05f2264db123504aebc5
Chris <unixdev73@gmail.com>
2026-05-10 13:12:14 +0200

    Add dynamic display
    Add date section
    Fix race conditions
    Refactor menu structure

--- include/wtts/interactiveCLI.hpp
+++ include/wtts/interactiveCLI.hpp
@@ -1,7 +1,10 @@
 #pragma once
 
+#include <atomic>
+#include <chrono>
 #include <functional>
 #include <iostream>
+#include <mutex>
 #include <wtts/employeeSystem.hpp>
 
 namespace es {
@@ -13,9 +16,99 @@ template <typename F> struct MenuEntry {
 using ShellMenuEntry = MenuEntry<std::function<void()>>;
 
 class Shell;
+// All build functions clear the existing menu
 void buildMainMenu(Shell *s);
-void buildGeneralAdminMenu(Shell *s);
 void buildAdminMenu(Shell *s);
+void buildManagerMenu(Shell *s);
+void buildDriverMenu(Shell *s);
+void buildEmployeeMenu(Shell *s);
+
+// All menu entry appending functions do not clear the existing menu
+void appendGeneralAdminMenuEntries(Shell *s);
+void appendAdminMenuEntries(Shell *s);
+void appendEmployeeMenuEntries(Shell *s);
+void appendDriverMenuEntries(Shell *s);
+
+template <typename T> struct GuardedResource {
+  mutable std::mutex guard;
+  T resource;
+
+public:
+  using value_type = T;
+
+  template <typename... Pack>
+  GuardedResource(Pack &&...args) : resource{std::forward<Pack>(args)...} {}
+
+  void set(T &&value) {
+    std::lock_guard<std::mutex> lock{guard};
+    resource = std::move(value);
+  }
+  void set(T const &value) {
+    std::lock_guard<std::mutex> lock{guard};
+    resource = value;
+  }
+
+  template <typename V> void push_back(V &&value) {
+    std::lock_guard<std::mutex> lock{guard};
+    resource.push_back(std::move(value));
+  }
+
+  template <typename V> void push_back(V const &value) {
+    std::lock_guard<std::mutex> lock{guard};
+    resource.push_back(value);
+  }
+
+  void pop_back() {
+    std::lock_guard<std::mutex> lock{guard};
+    resource.pop_back();
+  }
+
+  void clear() {
+    std::lock_guard<std::mutex> lock{guard};
+    resource.clear();
+  }
+
+  std::size_t size() const {
+    std::lock_guard<std::mutex> lock{guard};
+    return resource.size();
+  }
+
+  T get() const {
+    std::lock_guard<std::mutex> lock{guard};
+    return resource;
+  }
+
+  template <typename V> V merge() {
+    std::lock_guard<std::mutex> lock{guard};
+    V v;
+    for (auto const &e : resource)
+      v += e;
+    return v;
+  }
+
+  template <typename K> auto at(K const &key) {
+    std::lock_guard<std::mutex> lock{guard};
+    return resource.at(key);
+  }
+
+  operator T() const {
+    std::lock_guard<std::mutex> lock{guard};
+    return static_cast<T>(resource);
+  }
+};
+
+struct UserInterface {
+  GuardedResource<std::string> greeting;
+  GuardedResource<std::vector<ShellMenuEntry>> menu;
+  GuardedResource<std::string> inputInstruction;
+  GuardedResource<std::string> prompt;
+  GuardedResource<std::string> date;
+  GuardedResource<std::chrono::milliseconds> tick{
+      std::size_t((1.0 / 60.0) * 1000.0)};
+  GuardedResource<std::chrono::time_point<std::chrono::system_clock>>
+      lastRefresh;
+  GuardedResource<std::vector<char>> buf; // Stores input
+};
 
 class Shell {
 public:
@@ -23,33 +116,31 @@ public:
     buildMainMenu(this);
   }
 
-  void run();
-
-  void greet();
-
-  bool prompt();
-
-  std::size_t readIndex(std::string const &str);
-
-  std::string readLine();
-
   template <typename... P> void write(P &&...args) {
+    std::lock_guard<std::mutex> lock{consoleGuard_};
     (output_ << ... << args) << std::flush;
   }
-
-  std::vector<ShellMenuEntry> *getMenu() { return &menu_; }
-
+  void run();
+  void greet();
+  void renderUserInterface();
+  std::size_t readIndex();
+  std::string readLine();
   void requestExit() { exit_ = true; }
-
-  void setSystem(std::unique_ptr<EmployeeSystem> s) { system_ = std::move(s); }
-  EmployeeSystem *getSystem() { return system_.get(); }
-
-  void setPromptText(std::string s) { prompt_ = std::move(s); }
+  void setInputInstruction(std::string s) {
+    ui_.inputInstruction.set(std::move(s));
+  }
+  void setPromptText(std::string s) { ui_.prompt.set(std::move(s)); }
+  std::string getPromptText() const { return ui_.prompt.get(); }
 
   void setCurrentEmployeeId(std::string id) {
     currentEmployeeId_ = std::move(id);
   }
   std::string getCurrentEmployeeId() const { return currentEmployeeId_; }
+
+  void setSystem(std::unique_ptr<EmployeeSystem> s) { system_ = std::move(s); }
+  EmployeeSystem *getSystem() { return system_.get(); }
+
+  UserInterface *getInterface() { return &ui_; }
 
 private:
   std::unique_ptr<EmployeeSystem> system_{std::make_unique<EmployeeSystem>()};
@@ -57,10 +148,10 @@ private:
 
   std::istream &input_;
   std::ostream &output_;
-  std::string prompt_;
 
-  std::vector<ShellMenuEntry> menu_;
+  UserInterface ui_;
 
-  bool exit_{0};
+  std::mutex consoleGuard_;
+  std::atomic<bool> exit_{0};
 };
 } // namespace es
